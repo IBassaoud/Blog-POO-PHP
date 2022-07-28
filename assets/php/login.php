0840725c2b2d5496abce541205136d0484786592
ismail BASSAOUD <bwb-i.bassaoud@fondespierre.com>
2022-07-28 16:10:19 +0000

    Update style : overflow hidden login page

--- assets/php/login.php
+++ assets/php/login.php
@@ -66,10 +66,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['email'] != "" && $_POST['pass
     <script src="/assets/js/navbar.js"></script>
     <title>Sign in</title>
 </head>
-<body class="overflow-x-hidden">
+<body class="overflow-hidden">
 <header> 
   <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded white:bg-gray-900">
-    <div class="container flex flex-wrap justify-between items-center mx-auto">
+    <div class="container flex flex-wrap justify-between items-center mx-auto ">
       <a href="http://localhost:8006/" class="flex items-center">
           <img src="/assets/img/logo_blog.png" class="mr-3 h-6 sm:h-9"/>
           <span class="self-center text-xl font-semibold whitespace-nowrap white:text-black">The Blog</span>
@@ -129,7 +129,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['email'] != "" && $_POST['pass
 <section class="h-screen w-screen">
   <div class="px-6 h-full text-gray-800">
     <div
-      class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
+      class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6 overflow-y-hidden"
     >
       <div
         class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-11/12 mb-12 md:mb-0"
