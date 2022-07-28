9c43da1be044ab727891e7aac7603ada9e0d0913
ismail BASSAOUD <bwb-i.bassaoud@fondespierre.com>
2022-07-28 23:16:28 +0000

    added request to set timezone to Europe/paris at every connection to DB

--- pdo.php
+++ pdo.php
@@ -3,9 +3,14 @@
 $connex = "mysql:dbname=MYSQL_DATABASE;host=172.21.0.2:3306;";
 $user = "ism34";
 $pw = "admin";
+$root = "root";
+$rootpw = "MYSQL_ROOT_PASSWORD";
 
 try {
     $dbh = new PDO($connex, $user, $pw);
+    $set_timezone = "SET GLOBAL time_zone = 'Europe/Paris';";
+    $rootconn = new PDO($connex, $root, $rootpw);
+    $rootconn->query($set_timezone);
 }
 catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
